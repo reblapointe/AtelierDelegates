bd32bf70b68438075a01e01f82412f75a1a6ed30
RebeccaLapointe <reblapointe@gmail.com>
2022-02-22 14:13:40 -0500

    Code départ

--- AtelierDelegates/ResponsableDesCommunications.cs
+++ AtelierDelegates/ResponsableDesCommunications.cs
@@ -5,30 +5,20 @@ namespace AtelierDelegates
 {
     class ResponsableDesCommunications
     {
-        static bool continuer = true;
-
-        public static void Continuer(string message)
-        {
-            continuer = !message.ToUpper().Contains("QUE DIEU VOUS GARDE");
-        }
 
         public static void Main(string[] args)
         {
-            Action<string> archiviste = (m) => new Archiviste().Archiver(m);
-            Action<string> alarmiste = new Alarmiste().Crier;
-            Action<string> beeper = (m) => InterpreteurMalVoyant.Beep();
-            Action<string> journaliste = (m) => Journaliste.MettreALaUne("Le président", m);
-            Action<string> conspirationniste = (m) => Console.WriteLine(Conspirationniste.Ameliorer(m));
-            Action<string> deVinci = DeVinci.EcrireDeDroiteAGauche;
+            // Créer les délégués
+            Action<string> imprimerConsole = Console.WriteLine;
 
-            Action<string> auditeurs = archiviste + alarmiste + beeper + journaliste + conspirationniste + deVinci + Continuer;
+            // Rassembler les délégués
+            Action<string> auditeurs = imprimerConsole;
 
-            int messages = 0;
+            bool continuer = true;
+
             while(continuer)
             {
-                President.Parle(auditeurs);
-                if (++messages == 3)
-                    auditeurs -= alarmiste;
+                President.Parle(imprimerConsole);
             }
 
             Console.WriteLine("\n\n");
