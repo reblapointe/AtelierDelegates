fcdbfe8939ad567dcaa1b508f2b4b49ac0529ebd
RebeccaLapointe <reblapointe@gmail.com>
2023-01-11 13:46:25 -0500

    Code de départ

--- AtelierDelegates/ResponsableDesCommunications.cs
+++ AtelierDelegates/ResponsableDesCommunications.cs
@@ -4,26 +4,20 @@ namespace AtelierDelegates
 {
     class ResponsableDesCommunications
     {
-        public static void Main(string[] args)
+        public static void Main(string[] _)
         {
             bool continuer = true;
 
-            Action<string> archiviste = (m) => new Archiviste().Archiver(m);
-            Action<string> alarmiste = new Alarmiste().Crier;
-            Action<string> beeper = (m) => InterpreteurMalVoyant.Beep();
-            Action<string> journaliste = (m) => Journaliste.MettreALaUne("Le président", m);
-            Action<string> conspirationniste = (m) => Console.WriteLine(Conspirationniste.Ameliorer(m));
-            Action<string> deVinci = DeVinci.EcrireDeDroiteAGauche;
-            Action<string> phraseDeFin = (m) => continuer = !(m.Trim().ToUpper() == "LA TERRE AUX TERRIENS!");
+            // Création des délégués pour chaque auditeur
+            Action<string> console = Console.WriteLine;
 
-            Action<string> auditeurs = archiviste + alarmiste + beeper + journaliste + conspirationniste + deVinci + phraseDeFin;
+            // Combiner tous les délégués en un seul
+            Action<string> auditeurs = console;
 
-            int messages = 0;
+            // Faire parler le président
             while (continuer)
             {
                 President.Parler(auditeurs);
-                if (++messages == 3)
-                    auditeurs -= alarmiste;
             }
 
             Console.WriteLine("\n\n");
